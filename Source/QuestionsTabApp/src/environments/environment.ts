021084f09efec10a6f80f427036841e360ec2f78
sushantifi <sushant.chavan@ifi.tech>
2021-12-24 13:56:46 +0530

    v1.1

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://ifibotapp.azurewebsites.net/api/Request/",
+    selfUrl: "https://ifibotapp-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "6f0b33ae-4163-427b-a62d-941d3fa59d04",
+        clientId: "1e9b418e-9d6d-469a-8116-5e99715d98b8",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
